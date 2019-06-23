fda91b75d98ea2ef06e7b047f6c44d4ea57439e1
Jairo Jara Balboa <jairoandresjara@gmail.com>
2019-06-23 18:56:06 -0400

    MODELO LOGIN

--- src/java/DTO/Login.java
+++ src/java/DTO/Login.java
@@ -11,4 +11,33 @@ package DTO;
  */
 public class Login {
     
+    private int IdUser;
+    private String Password;
+
+    public Login() {
+    }
+
+    public Login(int IdUser, String Password) {
+        this.IdUser = IdUser;
+        this.Password = Password;
+    }
+
+    public int getIdUser() {
+        return IdUser;
+    }
+
+    public void setIdUser(int IdUser) {
+        this.IdUser = IdUser;
+    }
+
+    public String getPassword() {
+        return Password;
+    }
+
+    public void setPassword(String Password) {
+        this.Password = Password;
+    }
+    
+    
+    
 }
